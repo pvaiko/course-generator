3db3a4540517ad6249b4921e12d4d6d67c51a6d7
Peter Vajko <peter@vajko.name>
2017-06-14 20:23:02 -0500

    Removed dofiles, everything is loaded by CP
    Seems to be the only way to source all files needed,
    added them all to courseplay.lua.

--- cp.lua
+++ cp.lua
@@ -1,19 +1,8 @@
 --- This is the interface to Courseplay
 -- 
 
-local p = g_currentModDirectory .. '/course-generator/'
-
-dofile( p .. 'track.lua' )
-dofile( p .. 'file.lua' )
-dofile( p .. 'headland.lua' )
-dofile( p .. 'center.lua' )
-dofile( p .. 'geo.lua' )
-dofile( p .. 'bspline.lua' )
-
 
 function generate( vehicle, name, poly )
-
-  DebugUtil.printTableRecursively( vehicle.components, "  ", 1, 6 )
 
   local field = fromCpField( name, poly.points ) 
   calculatePolygonData( field.boundary )
