ebc2c51adab8e5777268a047c4007438ea477718
Peter Vajko <github@vajko.name>
2018-01-12 07:19:58 -0600

    Refactored new generation breeding
    Much better convergence now, more consistent results.

--- testGenetic.lua
+++ testGenetic.lua
@@ -25,15 +25,28 @@ assert( tostring( v1 ) == 'C-A-D-C-B-')
 v1:mutate( 1 )
 assert( tostring( v1 ) == 'C-A-D-C-D-')
 
+math.randomseed( 2 )
+local v2 = ValueEncodedChromosome:new( 5, genes )
+v2:fillWithRandomValues( )
+offspring = v1:crossover( v2 )
+assert( tostring( offspring ) == 'C-C-D-D-D-')
+
+math.randomseed( 1 )
 p1 = PermutationEncodedChromosome:newRandom( 4, genes )
 assert( tostring( p1 ) == 'C-A-D-B-')
 
 local maxGenerations = 200
-local mutationRate = 0.02
-local populationSize = 100
+
 local maxEliteRatio = 0.05
-local nGenes = 5
-local bestChromosome = { 'A', 'B', 'C', 'D', 'E', 'F'}
+local bestChromosome = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'}
+local bestBlockSequence = { 4, 2, 3, 1, 5, 6, 7 }
+local bestEntryCorner = { 1, 2, 3, 4, 1, 2, 3 }
+
+local nGenes = 6
+
+local validValues4 = { 'A', 'B', 'D', 'C' }
+local validValues6 = { 'A', 'B', 'D', 'C', 'E', 'F' }
+local validValues8 = { 'A', 'B', 'D', 'C', 'E', 'F', 'G', 'H' }
 
 function setBlockCorners( block, x1, y1, x2, y2, x3, y3, x4, y4 )
 	block.polygon[ courseGenerator.BLOCK_CORNER_BOTTOM_LEFT ] = { x = x1, y = y1 }
@@ -51,26 +64,26 @@ blocks[ 3 ] = { polygon = Polygon:new()}
 setBlockCorners( blocks[ 3 ], 11, 7, 15, 7, 15, 10, 11, 10 )
 blocks[ 4 ] = { polygon = Polygon:new()}
 setBlockCorners( blocks[ 4 ], 11, 0, 15, 0, 15, 3, 11, 3 )
-blocks[ 5 ] = { polygon = Polygon:new()}
-setBlockCorners( blocks[ 5 ], 0, 12, 10, 12, 10, 20, 0, 20 )
-blocks[ 6 ] = { polygon = Polygon:new()}
-setBlockCorners( blocks[ 6 ], 11, 17, 15, 17, 15, 20, 11, 20 )
-blocks[ 7 ] = { polygon = Polygon:new()}
-setBlockCorners( blocks[ 7 ], 16, 12, 20, 12, 20, 20, 16, 20 )
-
-math.randomseed( 1 )
-local p = Population:new()
-local validValues = { 'A', 'B', 'D', 'C', 'E', 'F' }
+--blocks[ 5 ] = { polygon = Polygon:new()}
+--setBlockCorners( blocks[ 5 ], 0, 12, 10, 12, 10, 20, 0, 20 )
+--blocks[ 6 ] = { polygon = Polygon:new()}
+--setBlockCorners( blocks[ 6 ], 11, 17, 15, 17, 15, 20, 11, 20 )
+--blocks[ 7 ] = { polygon = Polygon:new()}
+--setBlockCorners( blocks[ 7 ], 16, 12, 20, 12, 20, 20, 16, 20 )
 
 
---p:initialize( populationSize, function() return PermutationEncodedChromosome:newRandom( #genes, genes ) end )
-p:initialize( populationSize, function() 
-	local c = ValueEncodedChromosome:new( nGenes, validValues )
-	c:fillWithRandomValues()
-	return c
-	end )
 
-function calculateFitness( chromosome )
+function calculateFieldBlockFitness( chromosome )
+	local fitness = 0
+	for i = 1, #chromosome.blockSequence do
+		if chromosome.blockSequence[ i ] == bestBlockSequence[ i ] then fitness = fitness + 1 end
+		if chromosome.entryCorner[ chromosome.blockSequence[ i ]] == bestEntryCorner[ chromosome.blockSequence[ i ]] then fitness = fitness + 1 end
+ 	end
+	chromosome.fitness = fitness
+	return fitness
+end
+
+function calculateFitnessSimple( chromosome )
 	local fitness = 0
 	for i = 1, #chromosome do
 		if chromosome[ i ] == bestChromosome[ i ] then fitness = fitness + 1 end
@@ -79,39 +92,41 @@ function calculateFitness( chromosome )
 	return fitness
 end
 
-p:calculateFitness( calculateFitness )
-local generations = 1
-while p.bestFitness < nGenes do
-	local newPopulation = p:selectElite( maxEliteRatio )
-	while #newPopulation < populationSize do
-		local mother, father = p:selectParentsRouletteWheel()
-		offspring = mother:crossover( father )
-		offspring:mutate( mutationRate )
-		--print( mother, father, offspring )
-		table.insert( newPopulation, offspring )
+math.randomseed( os.time() )
+local generations = {}
+
+local mutationRate = 0.05
+local tournamentSize = 3
+local populationSize = 50
+
+local p = Population:new( calculateFieldBlockFitness, tournamentSize, mutationRate )
+for i = 1, 50 do
+
+	p:initialize( populationSize, function()
+		local c = FieldBlockChromosome:new( #blocks )
+		--local c = PermutationEncodedChromosome:new( nGenes, validValues8 )
+		--local c = ValueEncodedChromosome:new( nGenes, validValues6 )
+		c:fillWithRandomValues()
+		return c
+	end )
+	local generation = 1
+	p:calculateFitness()
+	while p.bestFitness < #blocks * 2 do
+		local newGeneration = p:breed()
+		p:recombine( newGeneration )
+		generation = generation + 1
 	end
-	generations = generations + 1
-	newPopulation:calculateFitness( calculateFitness )
-	print( generations, newPopulation.bestFitness )
-	p = newPopulation
+	table.insert( generations, generation )
+	print( generation, p.totalFitness, p.bestFitness )
 end
 print( p )
-print( generations, p.totalFitness )
-assert( p.totalFitness == 286 )
-assert( generations == 4 )
 
-math.randomseed( 1 )
-c = FieldBlockChromosome:new( 4 )
-c:fillWithRandomValues()
-print( c )
-assert( tostring( c ) == '3(4)-1(2)-4(4)-2(2)-' )
+print( "Min/max generations", math.min( table.unpack( generations )), math.max( table.unpack( generations )))
+--print( p )
 
-_, c = findBlockSequence(blocks)
-print( 'best', c )
-assert( tostring( c ) == '3-1-4-2-7-6-5-/3-1-1-4-3-1-1- f = 555.0, d = 18.0 m')
+--assert( p.totalFitness == 286 )
+--assert( generations == 4 )
 
-local oneBlock = { blocks[ 1 ]}
-_, c = findBlockSequence( oneBlock )
 
 
 print( "Done.")
